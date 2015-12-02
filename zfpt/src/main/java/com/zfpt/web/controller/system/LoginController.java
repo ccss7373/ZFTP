9287804028ea07b849e6763ec517d4e8444166b1
charles <chencheng73@163.com>
2015-12-02 21:53:30 +0800

    no message

--- zfpt/src/main/java/com/zfpt/web/controller/system/LoginController.java
+++ zfpt/src/main/java/com/zfpt/web/controller/system/LoginController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import com.zfpt.web.service.system.IUserService;
 
-/**      
+/**       
  * 项目名称：zfpt   
  * 类名称：LoginController   
  * 类描述： 系统登录控制器  
